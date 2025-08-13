d1b291548ebfe5a826549b5dc325390802175dcc
Arthur Gomes <arthurggomes1007@gmail.com>
2025-08-12 21:01:45 -0300

    11/08

--- exercicios.py
+++ exercicios.py
@@ -1,8 +1,26 @@
-soma = 0
-i = 0
-while i < 7:
-    idade = float(input("Idades:"))
-    i+=1
-    soma = idade + soma
-media = soma/7
-print("{:.2f}".format(media))
+idade=float(input("Digite a idade:"))
+peso=float(input("Digite o peso:"))
+
+if idade < 20:
+    if peso < 60:
+        print("9")
+    elif peso >= 60 and peso <= 90:
+        print("8")
+    elif peso > 90:
+        print("7")
+
+if idade >= 20 and idade <=50:
+    if peso < 60:
+        print("6")
+    elif peso >= 60 and peso <= 90:
+        print("5")
+    elif peso > 90:
+        print("4")
+
+if idade > 50:
+    if peso < 60:
+        print("3")
+    elif peso >= 60 and peso <= 90:
+        print("2")
+    elif peso > 90:
+        print("1")
