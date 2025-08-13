052ebedf45324b7cc7e9b79ac8832150b6fe9926
Arthur Gomes <arthurggomes1007@gmail.com>
2025-08-12 21:25:58 -0300

    11/08

--- exercicios.py
+++ exercicios.py
@@ -1,26 +1,10 @@
-idade=float(input("Digite a idade:"))
-peso=float(input("Digite o peso:"))
+soma = 0
+for i in range(1,6):
+    print(i)
+    periodo = float(input("Digite o periodo:"))
+    salario = int(input("Digite o salario"))
 
-if idade < 20:
-    if peso < 60:
-        print("9")
-    elif peso >= 60 and peso <= 90:
-        print("8")
-    elif peso > 90:
-        print("7")
+    if periodo == 3 or periodo == 5:
+        soma = soma + salario
 
-if idade >= 20 and idade <=50:
-    if peso < 60:
-        print("6")
-    elif peso >= 60 and peso <= 90:
-        print("5")
-    elif peso > 90:
-        print("4")
-
-if idade > 50:
-    if peso < 60:
-        print("3")
-    elif peso >= 60 and peso <= 90:
-        print("2")
-    elif peso > 90:
-        print("1")
+print("Soma dos salarios: ",soma)
